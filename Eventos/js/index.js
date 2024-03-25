e1bfb93b5b7a10a47149e137cabc734b120a61b2
Vinícius <138033752+vinimo12@users.noreply.github.com>
2024-03-25 10:20:59 -0300

    Add files via upload

---
+++ Eventos/js/index.js
@@ -0,0 +1,12 @@
+function oclk () {
+    document.getElementById("p1").innerHTML = "Olá esse texto foi alterado";
+}
+
+function omo () {
+    document.getElementById("p2").style.color = "#00ff00";
+}
+
+function onc () {
+    a = document.getElementById("i1");
+    a.value = a.value.toUpperCase();
+}
